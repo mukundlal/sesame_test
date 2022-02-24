e1b52b59cc25a08df5ff09acdf10c46545db6c29
mukundlal <mukundlaltnr@gmail.com>
2022-02-24 22:41:17 +0530

    fixes

--- app/src/main/java/com/cmlabs/sesame_test/ui/SplashActivity.kt
+++ app/src/main/java/com/cmlabs/sesame_test/ui/SplashActivity.kt
@@ -1,12 +1,18 @@
 package com.cmlabs.sesame_test.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import com.cmlabs.sesame_test.R
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+        Handler().postDelayed({
+          startActivity(Intent(this,MainActivity::class.java))
+            finish()
+        },1000)
     }
 }
